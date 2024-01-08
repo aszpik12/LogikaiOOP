5c062dccea0e69de954445b4bc6446a87cad3d62
Asztrik <105195963+aszpik12@users.noreply.github.com>
2024-01-08 10:44:43 +0100

    Lada, UML

---
+++ LogikaiOOP/src/logikaioop/Lada.java
@@ -0,0 +1,55 @@
+
+package logikaioop;
+    
+public class Lada {
+    String allitas, anyag;
+    boolean kincs;
+
+    public Lada() {
+        this("veszélyes", "fa", true);
+    }
+
+    
+    
+    public Lada(String allitas, String anyag, boolean kincs) {
+        this.allitas = allitas;
+        this.anyag = anyag;
+        this.kincs = kincs;
+    }
+
+    public String getAllitas() {
+        return allitas;
+    }
+
+    public String getAnyag() {
+        return anyag;
+    }
+
+    public boolean isKincs() {
+        return kincs;
+    }
+
+    public void setAllitas(String allitas) {
+        this.allitas = allitas;
+    }
+
+    public void setAnyag(String anyag) {
+        this.anyag = anyag;
+    }
+
+    public void setKincs(boolean kincs) {
+        this.kincs = kincs;
+    }
+
+    @Override
+    public String toString() {
+        return "Lada{" + "allitas=" + allitas + ", anyag=" + anyag + ", kincs=" + kincs + '}';
+    }
+
+    
+
+    
+    
+     
+    
+}
--- LogikaiOOP/src/logikaioop/LogikaiOOP.java
+++ LogikaiOOP/src/logikaioop/LogikaiOOP.java
@@ -6,11 +6,8 @@ public class LogikaiOOP {
 
   
     public static void main(String[] args) {
-     Logikai log = new Logikai("1 Opcio", "2 Opcio", "3 Opcio");
-      System.out.println("Leírás");
-        System.out.println("Az " + log.getOpcio1());
-        System.out.println("A " + log.getOpcio2());
-        System.out.println("A " + log.getOpcio3());
+        Lada lad = new Lada("gyors", "vas", true);
+        System.out.println("Leírás");
         System.out.println("Melyik:");
         System.out.println("A válasz jó | rossz");
     }
