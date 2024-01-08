50304c9355a7db970604c57d111ad6f663a281ec
Asztrik <105195963+aszpik12@users.noreply.github.com>
2024-01-08 10:29:56 +0100

    Update Logikai.java

--- LogikaiOOP/src/logikaioop/Logikai.java
+++ LogikaiOOP/src/logikaioop/Logikai.java
@@ -1,44 +1,6 @@
 
 package logikaioop;
-<<<<<<< HEAD
     
-public class Logikai {
-
-    String opcio1;
-    String opcio2;
-    String opcio3;
-
-    public Logikai(String opcio1, String opcio2, String opcio3) {
-        this.opcio1 = opcio1;
-        this.opcio2 = opcio2;
-        this.opcio3 = opcio3;
-    }
-
-    public String getOpcio1() {
-        return opcio1;
-    }
-
-    public String getOpcio2() {
-        return opcio2;
-    }
-
-    public String getOpcio3() {
-        return opcio3;
-    }
-
-    public void setOpcio1(String opcio1) {
-        this.opcio1 = opcio1;
-    }
-
-    public void setOpcio2(String opcio2) {
-        this.opcio2 = opcio2;
-    }
-
-    public void setOpcio3(String opcio3) {
-        this.opcio3 = opcio3;
-=======
-
-
 public class Logikai {
     String allitas1;
     String allitas2;
@@ -91,33 +53,12 @@ public class Logikai {
 
     public void setValasz(String valasz) {
         this.valasz = valasz;
->>>>>>> fejlesztes
     }
 
     @Override
     public String toString() {
-<<<<<<< HEAD
-        return "Logikai{" + "opcio1=" + opcio1 + ", opcio2=" + opcio2 + ", opcio3=" + opcio3 + '}';
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-        return (this == obj);
-    }
-
-    @Override
-    public int hashCode() {
-       int hash = 7;
-    return hash;
-    }
-
-
-
-    
-=======
         return "Logikai{" + "allitas1=" + allitas1 + ", allitas2=" + allitas2 + ", allitas3=" + allitas3 + ", valasztott=" + valasztott + ", valasz=" + valasz + '}';
     }
      
->>>>>>> fejlesztes
     
 }
