4863d08ce354ce28f8cb5905c63744c8d20d59ae
Asztrik <105195963+aszpik12@users.noreply.github.com>
2024-01-08 09:50:59 +0100

    mainben kiiras

--- LogikaiOOP/src/logikaioop/LogikaiOOP.java
+++ LogikaiOOP/src/logikaioop/LogikaiOOP.java
@@ -6,10 +6,11 @@ public class LogikaiOOP {
 
   
     public static void main(String[] args) {
+     Logikai log = new Logikai("1 Opcio", "2 Opcio", "3 Opcio");
       System.out.println("Leírás");
-        System.out.println("Az 1. állítás!");
-        System.out.println("Az 2. állítás!");
-        System.out.println("Az 3. állítás!");
+        System.out.println("Az " + log.getOpcio1());
+        System.out.println("A " + log.getOpcio2());
+        System.out.println("A " + log.getOpcio3());
         System.out.println("Melyik:");
         System.out.println("A válasz jó | rossz");
     }
