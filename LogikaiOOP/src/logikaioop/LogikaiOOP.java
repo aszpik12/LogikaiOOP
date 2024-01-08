88036fab53bd841d93688ebde4df827d97621883
Warhair <tegzesmarton@gmail.com>
2024-01-08 09:26:42 +0100

    scanner_megirasa

--- LogikaiOOP/src/logikaioop/LogikaiOOP.java
+++ LogikaiOOP/src/logikaioop/LogikaiOOP.java
@@ -4,16 +4,21 @@
  */
 package logikaioop;
 
+import java.util.Scanner;
+
 
 public class LogikaiOOP {
 
+    public static Scanner src = new Scanner(System.in);
   
     public static void main(String[] args) {
-      System.out.println("Leírás");
+       
+        System.out.println("Leírás");
         System.out.println("Az 1. állítás!");
         System.out.println("Az 2. állítás!");
         System.out.println("Az 3. állítás!");
         System.out.println("Melyik:");
+        int valasz = src.nextInt();
         System.out.println("A válasz jó | rossz");
     }
     
