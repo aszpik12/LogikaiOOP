fa31cb58a2c065e316c3444ff7e04c0143cd72d7
Warhair <tegzesmarton@gmail.com>
2024-01-08 11:22:13 +0100

    3.feladat_fugveny

--- LogikaiOOP/src/logikaioop/LogikaiOOP.java
+++ LogikaiOOP/src/logikaioop/LogikaiOOP.java
@@ -8,14 +8,27 @@ public class LogikaiOOP {
         public static Scanner src = new Scanner(System.in);
 
         public static void main(String[] args) {
+                run();
+                
+        }
 
-                System.out.println("Leírás");
+    private static void run() {
+       System.out.println("Leírás");
                 System.out.println("Az 1. állítás!");
                 System.out.println("Az 2. állítás!");
                 System.out.println("Az 3. állítás!");
                 System.out.println("Melyik:");
                 int valasz = src.nextInt();
-                System.out.println("A válasz jó | rossz");
+                Valasz(valasz);
+    }
+    private static void Valasz(int valasz){
+        if(valasz == 1){
+            System.out.println("A válasz jó");
         }
+        else{
+            System.out.println("A válasz rosz");
+        }
+        
+    }
 
 }
