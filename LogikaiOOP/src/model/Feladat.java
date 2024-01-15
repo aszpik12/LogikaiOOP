8c0c56fc37f912965bd9e621616211af68fa9d1b
Warhair <tegzesmarton@gmail.com>
2024-01-15 10:47:22 +0100

    model_frissitve

--- LogikaiOOP/src/model/Feladat.java
+++ LogikaiOOP/src/model/Feladat.java
@@ -9,11 +9,12 @@ ArrayList<Lada> ladak = new ArrayList<Lada>();
     Lada lad1 = new Lada("én rejtem a kincset", "Arany", false);
     Lada lad2 = new Lada("nem én rejtem a kincset", "Ezüst", true);
     Lada lad3 = new Lada("az arany hazudik", "Bronz", false);
-
+    konzol kon = new konzol();
     public Feladat() {
         ladak.add(lad1);
         ladak.add(lad2);
         ladak.add(lad3);
+        konzol.leiras();
        
         
     }
@@ -21,13 +22,14 @@ ArrayList<Lada> ladak = new ArrayList<Lada>();
         int valasz=scr.nextInt();
         return valasz;
     }
-    private String isHelyes(){
+    private void isHelyes(){
+        
         int valasz=valasz();
         if(ladak.get(valasz-1).getKincs() == true){
-            return "A válasz jó";
+            konzol.jovalasz();
         }
         else{
-            return "A válasz rosz";
+           konzol.rosszvalasz(); 
         }
     }
         
